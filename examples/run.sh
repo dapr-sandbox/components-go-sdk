91381fbfdad82004c1838d9df23daffa6b1b7557
Marcos Candeia <marrcooos@gmail.com>
2022-09-28 14:31:26 +0000

    Use dockercompose dependencies instead of a single docker-compose file
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- examples/run.sh
+++ examples/run.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 COMPONENT=${1:-state.memory}
 
+ADDITIONAL_ARGS=
+if [ -e $COMPONENT/docker-compose.dependencies.yml ]; then
+    ADDITIONAL_ARGS="-f $COMPONENT/docker-compose.dependencies.yml"
+fi
 cp $COMPONENT/component.yml components/.
 
-COMPONENT=$COMPONENT docker-compose build $ARGS
-COMPONENT=$COMPONENT docker-compose up
+COMPONENT=$COMPONENT docker-compose -f docker-compose.yml $ADDITIONAL_ARGS build $ARGS
+COMPONENT=$COMPONENT docker-compose -f docker-compose.yml $ADDITIONAL_ARGS up
